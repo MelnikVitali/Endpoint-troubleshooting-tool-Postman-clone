620b0f3662312e3c42cb80947a3f501b68a3b11c
Vitalii Melnik <mvitalim@gmail.com>
2023-10-06 00:55:02 +0300

    change theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import theme from './theme';
 import App from './components/App/App';
 
