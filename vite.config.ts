95ce3b90b300f49404fc07c63c96c31f9e33b23c
Vitalii Melnik <mvitalim@gmail.com>
2023-10-06 19:51:53 +0300

    add minify

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 1600,
-        rollupOptions: {
-            output: {
-                entryFileNames: '[name]-[hash].js', // If you need a specific file name, comment out
-                //chunkFileNames: 'js/[name]-[hash].js',
-            },
-        },
+  plugins: [react(), tsconfigPaths(), ViteMinifyPlugin()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js', // If you need a specific file name, comment out
+        //chunkFileNames: 'js/[name]-[hash].js',
+      },
     },
+  },
 
-    server: {
-        port: 3030,
-    },
-    preview: {
-        port: 8080,
-    },
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 8080,
+  },
 });
